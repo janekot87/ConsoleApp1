67e51ac9d5a4cc0e75cddce24d79b853fdeff59a
janekot87 <janekot87@gmail.com>
2019-02-17 17:30:52 +0200

    ignore file

---
+++ ConsoleApp1/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Event
+    {
+       
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsLive { get; set; }
+
+        public Event (int id, string name, int order, DateTime startDate, bool isLive)
+        {
+            Id = id;
+            Name = name;
+            Order = order;
+            StartDate = startDate;
+            IsLive = isLive;
+        }
+
+        public string FullName
+        {
+            get => FullName;
+
+            set => FullName = Name + StartDate;
+        }
+    }
+   
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var evt1 = new Event(12, "dffd1", 23, DateTime.Today,  true);
+            var evt2 = new Event(22, "dffd2", 33, DateTime.Today, false);
+            var evt3 = new Event(32, "dffd3", 43, DateTime.Today, true);
+           
+
+
+
+            List<Event> allEvents = new List<Event>();
+                allEvents.Add(evt1);
+                allEvents.Add(evt2);
+                allEvents.Add(evt3);
+
+            List<Event> evtlive = new List<Event>();
+            foreach (var i in allEvents)
+            {
+                if (i.IsLive)
+                {
+                    evtlive.Add(i);
+                    
+                }
+
+            }
+              foreach (var x in evtlive)
+              {
+
+
+                Console.WriteLine(x.Name);
+                Console.WriteLine(x.Id);
+                Console.WriteLine(x.IsLive);
+                Console.WriteLine(x.Order);
+                Console.WriteLine(x.StartDate);
+                Console.Write("\r\n");
+
+
+              }
+
+            List<string> evtNames = new List<string>();
+            foreach (var i in allEvents)
+            {
+                evtNames.Add(i.Name);
+            }
+
+            foreach (var a in evtNames)
+            {
+               Console.WriteLine(a);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*Console.WriteLine("Write the name");
+            string name =  Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int years = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello, "+ name +" "+ years+" years");
+            if (years <= 18)
+            {
+                Console.WriteLine("Good girl!");
+               
+            }
+            else
+            {
+                Console.WriteLine("Beautiful women!");
+            }*/
+
+
+
+
+
+
+
+        }
+    }
+}
